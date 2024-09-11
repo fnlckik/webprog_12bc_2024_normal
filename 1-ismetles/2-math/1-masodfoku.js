22dce6c748ecb2e07732ea1a265b108735cc0e0f
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-11 11:57:14 +0200

    1-masodfoku.js

--- 1-ismetles/2-math/1-masodfoku.js
+++ 1-ismetles/2-math/1-masodfoku.js
@@ -1,10 +1,13 @@
 function solveQuadratic(a, b, c) {
     const D = b*b - 4*a*c;
     if (D < 0) {
-        return 0;
+        return {};
     } else if (D === 0) {
-        return 1;
+        return {x: -b/(2*a)};
     } else {
-        return 2;
+        return {
+            x1: (-b-Math.sqrt(D))/(2*a),
+            x2: (-b+Math.sqrt(D))/(2*a)
+        };
     }
 }
