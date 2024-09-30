c3d8dce6f6f3ee9f1b1dd8446b35ce2e6e6134dd
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-30 10:21:31 +0200

    2-ko-papir-ollo randomItem(), computer choose

--- 1-ismetles/2-DOM/2-ko-papir-ollo/script.js
+++ 1-ismetles/2-DOM/2-ko-papir-ollo/script.js
@@ -1,3 +1,27 @@
+/*
+function randomItem() {
+    const r = Math.floor(Math.random() * 3); // 0, 1, 2
+    if (r === 0) {
+        return "ko";
+    } else if (r === 1) {
+        return "papir";
+    } else {
+        return "ollo";
+    }
+}
+*/
+
+// a-tól b-ig ad random egészet
+function randint(a, b) {
+    return Math.floor(Math.random() * (b-a+1)) + a;
+}
+
+function randomItem() {
+    const items = ["ko", "papir", "ollo"];
+    const r = randint(0, 2); // 0, 1, 2
+    return items[r];
+}
+
 // Ugyanazon eseménykezelő több objektumhoz is
 // hozzá van rendelve. A this objektum mondja meg, hogy
 // ki váltotta ki az eseményt!
@@ -9,6 +33,16 @@ function handleChoose() {
     for (const image of images) {
         image.removeEventListener("click", handleChoose);
     }
+
+    const computer = randomItem();
+    const img = document.createElement("img");
+    img.src = `images/${computer}.png`;
+    img.classList.add("img-height");
+    
+    const body = document.querySelector("body");
+    const p = document.querySelector("#eredmeny");
+    body.insertBefore(img, p); // szulo.insertBefore(gyerek, testver)
+
 }
 // classList => vissza adja az elem osztályainak "listáját" (DOMTokenList)
 // classList.add("active");
