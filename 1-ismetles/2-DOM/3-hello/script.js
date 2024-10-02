28ef3afcf28476fb1a4a29f077a1142de1a78739
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-02 12:17:42 +0200

    3-hello (userName)

--- 1-ismetles/2-DOM/3-hello/script.js
+++ 1-ismetles/2-DOM/3-hello/script.js
@@ -1,15 +1,17 @@
-function handleClick() {
-    const input = document.querySelector("input[type=text]");
-    
-    localStorage.setItem("user", input.value);
-
+function greetOldUser(userName) {
     const p = document.createElement("p");
-    p.innerText = `Szia ${input.value}!`; // "Szia FNL!"
+    p.innerText = `Szia ${userName}!`; // "Szia FNL!"
     const oldDiv = document.querySelector("#regi");
     oldDiv.appendChild(p);
 
     const newDiv = document.querySelector("#uj");
     newDiv.style.display = "none";
+}
+
+function handleClick() {
+    const input = document.querySelector("input[type=text]");
+    localStorage.setItem("user", input.value);
+    greetOldUser(input.value);
 }
 const button = document.querySelector("button");
 button.addEventListener("click", handleClick);
@@ -18,14 +20,9 @@ button.addEventListener("click", handleClick);
 function handleLoad() {
     // console.log("Betöltött az oldal!");
     const user = localStorage.getItem("user");
-    if (user !== null) {
-        const p = document.createElement("p");
-        p.innerText = `Szia ${user}!`; // "Szia FNL!"
-        const oldDiv = document.querySelector("#regi");
-        oldDiv.appendChild(p);
-
-        const newDiv = document.querySelector("#uj");
-        newDiv.style.display = "none";
+    // user !== null
+    if (user) {
+        greetOldUser(user);
     }
 }
 window.addEventListener("load", handleLoad);
