1fbc61f5bd7db659078f80b3b421924605e348de
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-06 12:07:24 +0100

    2-MARVEL

--- 2-esemenyek/1-delegalas/2-marvel/script.js
+++ 2-esemenyek/1-delegalas/2-marvel/script.js
@@ -8,12 +8,45 @@ for (const movie of movies) {
 }
 */
 
+let first = null;
+
+function swapMovies(li1, li2) {
+    const firstNumber = li1.innerText.split(". ")[0]; // "1"
+    const firstName = li1.innerText.split(". ")[1]; // "Vasember"
+
+    const secondNumber = li2.innerText.split(". ")[0]; // "2"
+    const secondName = li2.innerText.split(". ")[1]; // "Thor"
+    
+    li1.innerText = `${firstNumber}. ${secondName}`;
+    li2.innerText = `${secondNumber}. ${firstName}`;
+
+    li1.classList.add("swap");
+    li2.classList.add("swap");
+}
+
 // Delegálás
 // this: akihez az eseménykezelő tartozik (addEventListener) => <ul>
 // e.target: akivel az esemény történt! => <li>
 function handleClick(e) {
-    // console.log(this.innerText);
-    console.log(e.target);
+    const li = e.target;
+    if (li.matches("ul li")) {
+        // console.log(li);
+        if (!first) {
+            first = li;
+            first.classList.add("selected");
+        } else {
+            swapMovies(first, li);
+            first = null;
+        }
+    }
 }
 const ul = document.querySelector("ul");
 ul.addEventListener("click", handleClick);
+// elem.matches(selector): igaz, ha az elem illeszkedik a szelektorra
+
+function afterAnimation(e) {
+    console.log(e.animationName);
+    const li = e.target;
+    li.classList.remove("swap", "selected");
+}
+ul.addEventListener("animationend", afterAnimation)
