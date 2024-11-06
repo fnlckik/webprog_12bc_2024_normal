a0966eb762d4892a1477ae31506af9f6c24eb796
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-06 12:26:20 +0100

    3-aknakereso startGame, createBoard

--- 2-esemenyek/1-delegalas/3-aknakereso/script.js
+++ 2-esemenyek/1-delegalas/3-aknakereso/script.js
@@ -1,6 +1,6 @@
-const n = 12;
+const n = 12; // 12x12
 const mineCount = 10;
-let board = [];
+let board = []; // mátrix - játék állapottér, mezőket tartalmaz
 let revealedCount = 0;
 
 const table = document.querySelector("table");
@@ -11,3 +11,32 @@ function randint(a, b) {
     return Math.floor(Math.random() * (b-a+1)) + a;
 }
 
+// --------------------------------------------
+
+// Készítse el a kezdeti mátrixomat!
+// field = {isMine: ???, value: ???}
+function createBoard() {
+    for (let i = 0; i < n; i++) {
+        const row = [];
+        for (let j = 0; j < n; j++) {
+            const field = {
+                isMine: false,
+                value: 0
+            };
+            row.push(field);
+        }
+        board.push(row);
+    }
+}
+
+// A mátrix alapján jelenítse meg a táblázatot!
+function showBoard() {
+    
+}
+
+function startGame() {
+    createBoard();
+    showBoard();
+}
+const button = document.querySelector("button");
+button.addEventListener("click", startGame);
