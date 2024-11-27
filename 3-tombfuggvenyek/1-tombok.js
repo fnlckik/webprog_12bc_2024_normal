5663a571e6ccc13ec435500ffe9cf843a6b5f557
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-27 12:26:29 +0100

    1-tombok.js concat, spread, slice, join

--- 3-tombfuggvenyek/1-tombok.js
+++ 3-tombfuggvenyek/1-tombok.js
@@ -10,14 +10,30 @@ console.log("Módosítások:", a);
 
 // 1. Egyesítsük a tömböket!
 const b = [3, 4, 5];
-const c = [];
-const d = [];
+/*
+const c = []; // [11, 2, 7, 3, 4, 5]
+for (const e of a) {
+    c.push(e);
+}
+for (const e of b) {
+    c.push(e);
+}
+*/
+const c = a.concat(b); // [11, 2, 7, 3, 4, 5]
+const d = [...a, ...b]; // spread operator
 console.log("Összefésülve:", c, d);
 
 // 2. Résztömb: 2. elemtől 4. elemig (1. indextől 3. indexig)
+/*
 const e = [];
+for (let i = 1; i < 4; i++) {
+    e.push(c[i]);
+}
+*/
+// slice(a, b) esetén [a, b)
+const e = c.slice(1, 4); // [2, 7, 3]
 console.log("Résztömb:", e);
 
 // 3. Szöveggé alakítás: elválasztó szóköz legyen
-const f = [];
+const f = c.join(" ");
 console.log("Szöveggé:", f);
