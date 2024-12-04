da783b66bce2cc1e6c3989bd6ef5999f57a1e876
farkas.norbert <farkas.norbert@ckik.hu>
2024-12-04 08:46:22 +0100

    2-tetelek (some, every, find, toSorted)

--- 3-tombfuggvenyek/2-tetelek.js
+++ 3-tombfuggvenyek/2-tetelek.js
@@ -33,40 +33,64 @@ console.log("Négyzetek:", squares);
 
 // 2. Kiválogatás - Adjuk meg a negatív elemeket!
 // const negatives = x.filter(isNegative);
-// const evens = x.filter(e => e % 2 === 1);
+// const evens = x.filter(e => e % 2 === 0);
 const negatives = x.filter(e => e < 0);
 console.log("Negatívak:", negatives);
 
 // 3. Megszámolás - Negatívak száma
-const negativeCount = "";
+const negativeCount = x.filter(e => e < 0).length;
 console.log("Negatívak száma:", negativeCount);
 
 // 4. Eldöntés - Van-e páros?
-const anyEven = "";
+const anyEven = x.some(e => e % 2 === 0);
 console.log("Van-e páros:", anyEven);
 
 // 5. Eldöntés (optimista) - Mindegyik páros?
-const everyEven = "";
+const everyEven = x.every(e => e % 2 === 0);
 console.log("Mindegyik páros:", everyEven);
 
 // 6. Keresés
 // a) Hol van az első negatív? Ha nincs, akkor a -1 helyen van.
 // b) Mennyi az értéke? Ha nincs, akkor undefined.
-const firstNegative = "";
-const firstNegativeIndex = "";
-const lastNegative = ""; // !!! 2022 !!!
-const lastNegativeIndex = "";
-console.log("Első negatív (hely, érték):", firstNegativeIndex, firstNegative);
-console.log("Utolsó negatív (hely, érték):", lastNegativeIndex, lastNegative);
+const firstNegative = x.find(isNegative);
+const firstNegativeIndex = x.findIndex(isNegative);
+const lastNegative = x.findLast(isNegative); // !!! 2022 !!!
+const lastNegativeIndex = x.findLastIndex(isNegative);
+console.log("Első negatív (hely, érték):", firstNegativeIndex+1, firstNegative);
+console.log("Utolsó negatív (hely, érték):", lastNegativeIndex+1, lastNegative);
 
 
 
 // ------------------------------------------------------
+function compare(a, b) {
+    if (a < b) {
+        return -911;
+    } else if (a > b) {
+        return 122;
+    } else {
+        return 0;
+    }
+}
+
+function compare2(a, b) {
+    /*
+    if (a < b) {
+        return a-b; // negativ < 0
+    } else if (a > b) {
+        return a-b; // pozitiv > 0
+    } else {
+        return a-b; // 0
+    }
+    */
+    return a - b;
+}
+
 // 7. Rendezzük az elemeket növekvő sorrendbe:
-const sorted = []; // !!! 2023 !!!
+const sorted = x.toSorted((a, b) => a - b); // !!! 2023 !!!
 console.log("Növekvő sorrend:", sorted);
 
-// Comparer function: (a, b)-ről képez le -1, 0, 1 elemekre
+
+// Comparer function: (a, b)-ről képez le -, 0, + elemekre
 // -1, ha a < b
 // 0, ha a === b
 // 1, ha a > b
