5df53b57048c87af44d11d52fc150ed9333b5336
farkas.norbert <farkas.norbert@ckik.hu>
2024-12-18 12:26:43 +0100

    Date functions

--- 4-aszinkronitas/1-ido/script.js
+++ 4-aszinkronitas/1-ido/script.js
@@ -7,3 +7,22 @@ button.addEventListener("click", increase);
 
 // ----------------------------------
 
+// Hány ms (ezredmásodperc) telt el 1970-01-01 óta?
+const timestamp = Date.now(); // number, ~ statikus metódus
+const today = new Date(); // object, ~ példányosítás (kb. Date osztály)
+const ww2 = new Date(1939, 8, 1); // hónap: 0-tól 11-ig
+const christmas = new Date("2024-12-24");
+const UNIX = new Date(0);
+const time = new Date("2024-12-18 12:08:37");
+
+// today.getDay(): A hét hanyadik napja?
+const year = today.getFullYear();
+const month = today.getMonth() + 1; // 0-tól 11-ig számoljuk
+const day = today.getDate();
+
+const hour = today.getHours();
+const minute = today.getMinutes();
+const second = today.getSeconds();
+
+const dateSpan = document.querySelector("#datum");
+dateSpan.innerText = `${year}-${month}-${day}`;
