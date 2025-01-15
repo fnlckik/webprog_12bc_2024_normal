4cd99b8151a05d98556651aaf05bbd02809e3833
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-15 08:39:50 +0100

    Feleves-doga (megoldas)

--- dolgozatok/Felevi-n/script.js
+++ dolgozatok/Felevi-n/script.js
@@ -51,21 +51,72 @@ showConcerts(concerts);
 // -------------------------------------------
 
 // F1
+function randint(a, b) {
+    return Math.floor(Math.random() * (b-a+1)) + a;
+}
 
+// early return
+function handleMatrix(e) {
+    if (e.key !== "r") return;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 3; j++) {
+            data[i][j] = randint(10, 99);
+        }
+    }
+    showTable(data);
+}
+window.addEventListener("keyup", handleMatrix);
 
 
 // F2
-
+function handleClick(e) {
+    const element = e.target;
+    if (!element.matches("td")) return;
+    const j = element.cellIndex;
+    const i = element.parentNode.rowIndex;
+    element.innerText = (i+1)*(j+1);
+    element.classList.add("chosen");
+}
+table.addEventListener("click", handleClick);
 
 
 // F3
-
-
+const span = document.querySelector("#average");
+const sum = concerts.reduce((s, concert) => s + concert.audience, 0);
+span.innerText = (sum / concerts.length).toFixed(0);
+span.style.fontWeight = "bold";
 
 // F4
+function compare(a, b) {
+    if (a.audience > b.audience) {
+        return 1;
+    } else if (a.audience < b.audience) {
+        return -1;
+    } else if (a.location > b.location) { // innentől a közönség ugyanannyi
+        return 1;
+    } else if (a.location < b.location) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
 
+const button = document.querySelector("#menu button");
+function handleSort() {
+    const sortedConcerts = concerts.toSorted(compare);
+    // const sortedConcerts = concerts.toSorted((a, b) => a.audience - b.audience);
+    showConcerts(sortedConcerts);
+}
+button.addEventListener("click", handleSort);
 
 
 // F5
-
+const input = document.querySelector("#menu input");
+function filterConcerts() {
+    const value = parseInt(input.value);
+    const x = concerts.filter(concert => concert.audience >= value);
+    const locations = x.map(concert => concert.location);
+    console.log(locations);
+}
+input.addEventListener("change", filterConcerts);
 
