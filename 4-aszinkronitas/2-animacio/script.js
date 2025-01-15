54d6d1f84192603a704f2b1ea84aa0dbc072ad8a
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-15 18:08:01 +0100

    idomeres

--- 4-aszinkronitas/2-animacio/script.js
+++ 4-aszinkronitas/2-animacio/script.js
@@ -22,12 +22,47 @@ const loading = document.querySelector("#loading div");
 loading.style.width = "0%";
 function updateLoading() {
     let width = parseInt(loading.style.width); // "12%" => 12
-    width += randint(0, 5); // 12 => 14
+    width += randint(1, 5); // 12 => 14
     // if (width > 100) width = 100;
     width = Math.min(width, 100);
     loading.style.width = width + "%"; // "14%"
     if (width < 100) {
-        setTimeout(updateLoading, 50);
+        setTimeout(updateLoading, randint(10, 500));
     }
 }
-updateLoading();
+// updateLoading();
+
+const id = setInterval(() => {
+    let width = parseInt(loading.style.width); // "12%" => 12
+    width += randint(1, 5); // 12 => 14
+    width = Math.min(width, 100);
+    loading.style.width = width + "%"; // "14%"
+    // console.log("Fut az időzítő!");
+    if (width === 100) {
+        clearInterval(id);
+    }
+}, 50);
+
+// F3
+const start = Date.now();
+function updateTime() {
+    const elapsed = Date.now() - start;
+    console.log(elapsed);
+    if (elapsed < 5000) {
+        setTimeout(updateTime, 1);
+    }
+}
+updateTime();
+
+// Vigyázat! A setTimeout() nem pontos ezredmásodpercre!
+let db = 0;
+function precision() {
+    const start = Date.now();
+    db++;
+    if (db < 100) {
+        setTimeout(() => {
+            precision();
+            console.log(Date.now() - start);
+        }, 1);
+    }
+}
