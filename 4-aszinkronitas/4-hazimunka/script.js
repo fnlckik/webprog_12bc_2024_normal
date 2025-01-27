d208ee0efa8aa7d058bcc51e4839e416d675d5da
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-27 10:05:43 +0100

    4-hazimunka

--- 4-aszinkronitas/4-hazimunka/script.js
+++ 4-aszinkronitas/4-hazimunka/script.js
@@ -12,15 +12,23 @@ function washDishes(callback) {
     }, 1000);
 }
 
-function takeTrash() {
+function takeTrash(callback) {
     setTimeout(() => {
-        // ???
+        callback("Szemét!")
     }, 100);
 }
 
-walkDog(message => {
-    console.log(message);
-    washDishes(message => {
+// callback hell (pyramid of doom)
+function chores() {
+    walkDog(message => {
         console.log(message);
-    })
-});
+        washDishes(message => {
+            console.log(message);
+            takeTrash(message => {
+                console.log(message);
+                console.log("Kész!");
+            })
+        })
+    });
+}
+chores();
