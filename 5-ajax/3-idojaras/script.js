b3904a71b2681a4315670c40fc9b04655c3794e5
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-19 12:25:53 +0100

    weather API (key)

---
+++ 5-ajax/3-idojaras/script.js
@@ -0,0 +1,11 @@
+// User interface: felhasználói felület
+// interface: felület
+// API: Application Programming Interface
+
+// https://www.weatherapi.com/docs/
+
+const APIKEY = "c75e43014d424a7a92a231046251802";
+
+// http://api.weatherapi.com/v1/current.json?key=c75e43014d424a7a92a231046251802&q=Paris
+
+const URL = "http://api.weatherapi.com/v1";
