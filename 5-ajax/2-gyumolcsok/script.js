49bac314e2d787285b613212a7820cc74afd71e6
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-19 08:45:55 +0100

    products (show, createCard)

--- 5-ajax/2-gyumolcsok/script.js
+++ 5-ajax/2-gyumolcsok/script.js
@@ -1,9 +1,40 @@
 // https://hur.webmania.cc/products.json
 
+function createCard(product) {
+    const div = document.createElement("div"); // Kártya!
+    
+    const img = document.createElement("img");
+    img.src = product.picture;
+    div.appendChild(img);
+
+    const pNev = document.createElement("p");
+    pNev.innerText = `Név: ${product.name}`;
+    div.appendChild(pNev);
+
+    const pAr = document.createElement("p");
+    pAr.innerText = `Ár: ${product.price} Ft`
+    div.appendChild(pAr);
+
+    return div;
+}
+
+function show(data) {
+    const ul = document.querySelector("ul");
+    const products = data.products;
+    products.forEach(product => {
+        const li = document.createElement("li");
+        const div = createCard(product);
+        li.appendChild(div);
+        ul.appendChild(li);
+    });
+}
+
+let data;
 const button = document.querySelector("button");
 async function getFruits() {
     const response = await fetch("https://hur.webmania.cc/products.json");
-    const data = await response.json();
+    data = await response.json();
     console.log(data);
+    show(data);
 }
 button.addEventListener("click", getFruits);
