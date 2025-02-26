05593e60235767d0a74ed4dcb837150ea0135ba8
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-26 12:28:37 +0100

    Időjárás - weather API (current)

--- 5-ajax/3-idojaras/script.js
+++ 5-ajax/3-idojaras/script.js
@@ -9,3 +9,30 @@ const APIKEY = "c75e43014d424a7a92a231046251802";
 // http://api.weatherapi.com/v1/current.json?key=c75e43014d424a7a92a231046251802&q=Paris
 
 const URL = "http://api.weatherapi.com/v1";
+
+function show(data) {
+    const div = document.querySelector("div");
+    div.innerHTML = `
+        <p>Város: ${data.location.name}</p>
+        <p>Ország: ${data.location.country}</p>
+        <p>Hőmérséklet: ${data.current.temp_c} °C</p>
+        <p>Csapadék: ${data.current.precip_mm} mm</p>
+    `;
+}
+
+const button = document.querySelector("button");
+async function handleClick() {
+    const input = document.querySelector("input");
+    const city = input.value;
+    try {
+        const response = await fetch(`${URL}/current.json?q=${city}&key=${APIKEY}`);
+        const data = await response.json();
+        if (!response.ok) {
+            throw new Error("Nincs ilyen város!");
+        }
+        show(data);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+button.addEventListener("click", handleClick);
