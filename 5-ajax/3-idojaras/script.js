328fdf447657c615120a2bd4317913367c0a8a8b
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-05 08:45:40 +0100

    current weather

--- 5-ajax/3-idojaras/script.js
+++ 5-ajax/3-idojaras/script.js
@@ -10,29 +10,44 @@ const APIKEY = "c75e43014d424a7a92a231046251802";
 
 const URL = "http://api.weatherapi.com/v1";
 
-function show(data) {
+function showCurrent(data) {
     const div = document.querySelector("div");
     div.innerHTML = `
         <p>Város: ${data.location.name}</p>
         <p>Ország: ${data.location.country}</p>
         <p>Hőmérséklet: ${data.current.temp_c} °C</p>
         <p>Csapadék: ${data.current.precip_mm} mm</p>
+        <p>Páratartalom: ${data.current.humidity}%</p>
+        <p id="egbolt">
+            <span>Égbolt: ${data.current.condition.text}</span>
+            <img src="https:${data.current.condition.icon}">
+        </p>
     `;
 }
 
-const button = document.querySelector("button");
-async function handleClick() {
+const buttonCurrent = document.querySelector("#aktualis");
+async function handleCurrent() {
     const input = document.querySelector("input");
     const city = input.value;
     try {
-        const response = await fetch(`${URL}/current.json?q=${city}&key=${APIKEY}`);
+        const response = await fetch(`${URL}/current.json?q=${city}&key=${APIKEY}&lang=hu`);
         const data = await response.json();
         if (!response.ok) {
             throw new Error("Nincs ilyen város!");
         }
-        show(data);
+        showCurrent(data);
     } catch (error) {
         console.log(error.message);
     }
 }
-button.addEventListener("click", handleClick);
+buttonCurrent.addEventListener("click", handleCurrent);
+
+// -------------------------------------
+
+const buttonForecast = document.querySelector("#aktualis");
+function handleForecast() {
+    const input = document.querySelector("input");
+    const city = input.value;
+    fetch(`${URL}/foreacast.json?q=${city}&key=${APIKEY}`);
+}
+buttonForecast.addEventListener("click", handleForecast);
