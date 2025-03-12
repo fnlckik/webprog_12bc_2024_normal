4761f94d747f40073f321d1ddc673590fb6266bb
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-12 08:20:25 +0100

    omdb (fetch)

--- 5-ajax/4-filmek/script.js
+++ 5-ajax/4-filmek/script.js
@@ -6,3 +6,34 @@ const URL = "http://www.omdbapi.com/";
 
 // http://www.omdbapi.com/?apikey=14051e6b&t=Dune
 
+function show(data) {
+    const div = document.querySelector("div");
+    div.innerHTML = `
+        <p>Cím: ${data.Title}</p>
+        <p>Értékelés (imdb): ${data.imdbRating}</p>
+    `;
+}
+
+async function getMoviesAsync() {
+    const input = document.querySelector("input");
+    const movie = input.value;
+    const response = await fetch(`${URL}?apikey=${APIKEY}&t=${movie}`);
+    const data = await response.json();
+    console.log(data);
+    show(data);
+}
+
+// function getMoviesXHR() {
+//     const input = document.querySelector("input");
+//     const movie = input.value;
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET", `http://www.omdbapi.com/?apikey=f85a5e67&t=${movie}`)
+//     xhr.onload = () => {
+//         const data = JSON.parse(xhr.response)
+//         show(data);
+//     }
+//     xhr.send();
+// }
+
+const button = document.querySelector("button");
+button.addEventListener("click", getMoviesAsync);
