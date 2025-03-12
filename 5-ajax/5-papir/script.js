7eccae98b0f658f987e7c6a79763d5488a81d5a9
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-12 12:25:31 +0100

    papir (POST)

--- 5-ajax/5-papir/script.js
+++ 5-ajax/5-papir/script.js
@@ -17,14 +17,42 @@ keresButton.addEventListener("click", getData);
 
 // -----------------------------------------------------
 // Adatok küldése
+// fetch("http://localhost/papir/create/index.php")
+// const adat = { osztaly, tomeg }
 const kuldesButton = document.querySelector("#kuldes");
-function sendDataFetch() {
+async function sendDataFetch() {
     const osztaly = document.querySelector("#osztaly-kuldes").value;
     const tomeg = document.querySelector("#tomeg-kuldes").value;
+    const adat = {
+        osztaly: osztaly,
+        tomeg: tomeg
+    };
+    const OPTIONS = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(adat)
+    };
+    const response = await fetch("http://localhost/papir/create/", OPTIONS);
+    const data = await response.json();
+    console.log(data);
 }
 
 function sendDataXHR() {
     const osztaly = document.querySelector("#osztaly-kuldes").value;
     const tomeg = document.querySelector("#tomeg-kuldes").value;
+    const adat = {
+        osztaly: osztaly,
+        tomeg: tomeg
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST", "http://localhost/papir/create/");
+    xhr.onload = () => {
+        const data = JSON.parse(xhr.response); // JS objektumot ad vissza
+        console.log(data);
+    };
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(adat));
 }
 kuldesButton.addEventListener("click", sendDataXHR);
